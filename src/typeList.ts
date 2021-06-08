8e0321688255a0b25af9bb042a622a18a4055c69
Takanobu <61861946+takanobu68@users.noreply.github.com>
2021-06-08 13:53:41 +0900

    引数の型指定

--- src/typeList.ts
+++ src/typeList.ts
@@ -4,3 +4,34 @@
 
 // boolean
 let bool: boolean = true;
+
+// number
+let num: number = 0;
+
+// string
+let str: string = "A";
+
+// Array
+let arr1: Array<number> = [0, 1, 2];
+let arr2: number[] = [0, 1, 2];
+
+// tuple
+let tuple: [number, string] = [0, "A"];
+
+// any
+let any1: any = false;
+
+// void
+const funk1 = (): void => {
+  const test = "TEST";
+};
+
+// null
+let null1: null = null;
+
+// undefined
+let undefined1: undefined = undefined;
+
+// object
+let obj1: object = {};
+let obj2: { id: number; name: string } = { id: 0, name: "AAA" };
